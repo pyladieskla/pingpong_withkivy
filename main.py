f7484c566baed17fd016f7341106834ec9faef26
arthur Kateeba <arthurkateeba@gmail.com>
2022-11-12 14:27:05 +0300

    make changes to application file main

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import kivy
 kivy.require('2.1.0')
 
-from kivy.app import app
-from kivy.uix.label import label
+from kivy.app import App
+from kivy.uix.Label import Label
 
 class PongGame(Widget):
 	pass
 
 class PongApp(App):
 	def build(self):
-		rethrn PongGame()
+		return PongGame()
 
 if __name__ == '__main__':
 	PongApp().run()
